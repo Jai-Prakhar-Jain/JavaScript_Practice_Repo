ecb7450563904c9df87c898043963565b4586729
Jai Prakhar Jain <jaiprakharjain@gmail.com>
2025-04-11 12:36:53 +0530

    Understanding JavaScript Variables

---
+++ beginner/variables.js
@@ -0,0 +1,17 @@
+const constVariable = 6215;
+let letVariable = "Jai";
+var varVariable = "Prakhar";
+variable = "Jain";
+console.table([constVariable, letVariable, varVariable, variable]);
+
+// constVariable = 9838;  const --> it's value cannot be changed once declared
+letVariable = "JAI";
+varVariable = "PRAKHAR";
+variable = "JAIN";
+
+// console.log(constVariable);
+// console.log(letVariable);
+// console.log(varVariable);
+// console.log(variable);
+
+console.table([constVariable, letVariable, varVariable, variable]);
