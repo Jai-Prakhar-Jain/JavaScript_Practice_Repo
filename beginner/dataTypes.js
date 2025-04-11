40dfb36c356720eca8d772cb0025256c569935d3
Jai Prakhar Jain <jaiprakharjain@gmail.com>
2025-04-11 20:57:17 +0530

    Data Types In JavaScript

---
+++ beginner/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // This Is Used To Treat The Below JS Code As New JavaScript Code
+
+/*
+Primitive Data Types
+1.) number --> Max Range(2^53)
+2.) string
+3.) bigInt
+4.) boolean
+5.) null 
+6.) undefined
+7.) symbol --> Unique
+*/
+
+/*
+Non-Primitive Data Type
+1.) object
+*/
+
+console.log(typeof null); //Output : object
+console.log(typeof undefined); //Output : undefined
