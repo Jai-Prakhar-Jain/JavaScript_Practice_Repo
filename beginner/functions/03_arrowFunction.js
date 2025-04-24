22de0f1e04fed063d5c9eb47daaf5fc2ab76c236
Jai Prakhar Jain <JaiPrakhar.Jain@Geminisolutions.com>
2025-04-24 12:34:25 +0530

    Arrow Function

--- beginner/functions/03_arrowFunction.js
+++ beginner/functions/03_arrowFunction.js
@@ -2,5 +2,11 @@ const sampleArrowFunction = (parameter1, parameter2)=>
 {
     return parameter1+parameter2
 }
-console.log(sampleArrowFunction(1,2));
-console.log(sampleArrowFunction("Hello"," World"));
+// console.log(sampleArrowFunction(1,2));
+// console.log(sampleArrowFunction("Hello"," World"));
+
+const sampleArrowFunction1 = (parameter1, parameter2)=> parameter1+parameter2  // There is no need to use {} and return keyword if you only have return statement in your arrow function  
+// console.log(sampleArrowFunction1(2,3));
+const sampleArrowFunction2 = ()=> ({"firstName": "Jai", "lastName":"Jain"}) // To return an object from arrow function parantheses are used () and object is defined inside the ()
+console.log(sampleArrowFunction2());
+
